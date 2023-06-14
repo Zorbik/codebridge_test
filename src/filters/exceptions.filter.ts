318af3e7be36b7ea75a41a6b453a785fba909dd5
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2023-06-14 22:05:21 +0300

    add error handler

---
+++ src/filters/exceptions.filter.ts
@@ -0,0 +1,29 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    let statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+
+    if (exception instanceof HttpException) {
+      statusCode = exception.getStatus();
+      message = exception.getResponse() as string;
+    }
+
+    response.status(statusCode).json({
+      statusCode,
+      message,
+    });
+  }
+}
